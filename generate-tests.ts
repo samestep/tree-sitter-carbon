f259beb29ef353a45c22dae506eca80e1ca31c40
Sam Estep <sam@samestep.com>
2024-10-31 10:06:33 -0400

    Use path to source file as name to ease debugging

--- generate-tests.ts
+++ generate-tests.ts
@@ -7,10 +7,10 @@ await fs.rm(out, { recursive: true });
 for (const file of await fs.readdir(src, { recursive: true })) {
   const parsed = path.parse(file);
   if (parsed.ext === ".carbon") {
-    const renamed = path.format({ ...parsed, base: "", ext: ".txt" });
-    const name = path.format({ ...parsed, base: "", ext: "" });
-    const contents = await Bun.file(`${src}/${file}`).text();
+    const name = `${src}/${file}`;
+    const contents = await Bun.file(name).text();
     const test = `===\n${name}\n===\n\n${contents}\n---\n`;
+    const renamed = path.format({ ...parsed, base: "", ext: ".txt" });
     await Bun.write(`${out}/${renamed}`, test);
   }
 }
